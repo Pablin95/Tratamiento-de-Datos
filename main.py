5a7eb99faec67b999f072941f2b5f826795f8b81
dromeroar <daromeroar@uide.edu.ec>
2022-08-22 23:38:05 -0500

    bot

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
-# This is a sample Python script.
+from bs4 import BeautifulSoup
+import requests
+import schedule
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def bot_send_text(bot_message):
+    bot_token = 'bot5400299704:AAFDm3QkSdd86zZlfWx0xSY_yeZDgyRjX6'
+    bot_chatID = '873567754'
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
 
-'''comentario de prueba J.'''
+    response = requests.get(send_text)
 
-# Press the green button in the gutter to run the script.
+    return response.json()
+
+def btc_scraping():
+    url = requests.get('https://awebanalysis.com/es/coin-details/bitcoin/')
+    soup = BeautifulSoup(url.content, 'html.parser')
+    result = soup.find('td', {'class': 'text-larger text-price'})
+    format_result = result.text
+
+    return format_result
+
+def report():
+    btc_price = f'El precio de Bitcoin es de {btc_scraping()}'
+    bot_send_text(btc_price)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+
+    schedule.every().day.at("23:31").do(report)
+
+    """while True:
+        schedule.run_pending()"""
+# Press the green button in the gutter to run the script.
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
